e5d922557139b757189e0a4a6675761f8647dcb9
denius97 <106709563+denius97@users.noreply.github.com>
2024-09-19 22:17:39 +0300

    add response code

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -42,12 +43,14 @@ export const setupServer = () => {
 
     if (!contact) {
       res.status(404).json({
+        status: 404,
         message: 'Contact not found',
       });
       return;
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
