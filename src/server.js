082584d3984de62343b203cf06954574d1ef9d70
denius97 <106709563+denius97@users.noreply.github.com>
2024-09-19 22:26:07 +0300

    add status code

--- src/server.js
+++ src/server.js
@@ -58,12 +58,14 @@ export const setupServer = () => {
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
   });
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
