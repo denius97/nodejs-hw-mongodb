78308f54ce37f446b7ae59ed189c9c3a22a9347a
denius97 <106709563+denius97@users.noreply.github.com>
2024-10-25 00:30:48 +0300

    Swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = env('PORT', 3000);
 
@@ -33,6 +34,7 @@ export const setupServer = () => {
   });
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
